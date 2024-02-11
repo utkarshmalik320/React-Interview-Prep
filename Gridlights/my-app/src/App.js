5d5f5307a16b74e953cdd651c9eebe670e4b3920
Utkarsh Malik <utkarshmalik320@gmail.com>
2024-02-11 19:24:40 +0530

    v3.1

--- Gridlights/my-app/src/App.js
+++ Gridlights/my-app/src/App.js
@@ -1,11 +1,75 @@
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+
+function Cell ({filled, onClick , isDisabled, label}){
+  return ( 
+   <button
+    type ='button'
+    aria-label={label}
+    disabled = {isDisabled}
+    onclick = {onClick}
+    className = {filled  ? "cell cell-activated " : "cell"}
+  />
+  )
+}
+
+export default  function App() {
+  const[ order, setOrder ] = useState([])
+  const [isdeactivated, setIsdeactivated] = useState(false)
+  const config = [
+    [1,1,1],
+    [1,0,1],
+    [1,1,1],
+  ];
+
+  const deactiateCells  = () =>{
+    setIsdeactivated(true);
+    const timer  =  setInterval(() => {
+        setOrder((originalOrder) =>{
+          const neworder = originalOrder.slice()
+          neworder.pop();
+
+          if(neworder.length === 0){
+            clearInterval(timer)
+            setIsdeactivated(false);
+          }
+
+          return neworder;
+        })
+    }, 300);
+  };
+
+  const activateCells = (index) =>{
+        const neworder = [...order , index]
+        setOrder(neworder)
+       
+   
+
+        if(neworder.length === config.flat(1).filter(Boolean).length){
+          deactiateCells();
+        }
+  };
   return (
-    <div className="App">
-        App
+    <div className="wrapper">
+      <div 
+      className ="grid" 
+      style = {{
+        gridTemplateColumns : `repeat(${config[0].length} , 1fr)`,
+      }}>
+
+        {config.flat(1).map((value, index )=>{
+          return value ?  ( <Cell
+          key = {index}
+          label  = {`cell ${index}`}
+          filled = {order.includes(index)}
+          onclick = { () => {activateCells(index)}}
+          isDisabled = {order.includes(index) || isdeactivated}
+          />): (<span/>);
+        })}
+      </div>
     </div>
   );
 }
 
-export default App;
+
